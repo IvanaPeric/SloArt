56b8952c1826feb74c1f2b2dcd191b723773bc4a
sreten peric <sreten@sreten.sreten>
2023-06-06 16:54:45 +0200

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./*.{html,cjs}"],
+  theme: {
+    extend: {
+      colors: {
+        clr_primary: '#222222',
+        clr_monochrome_dark: '#888888',
+        clr_monochrome_gray: '#BBBBBB',
+        clr_white: '#FFFFFF',
+        clr_black: '#000000'
+      },
+      fontSize: {
+        'sm': '12px',
+        'base': '16px',
+        'lg': '24px',
+        'xl': '32px',
+        '2xl': '48px'
+      }
+    }
+  },
+  plugins: [],
+}
+
+
